7319073ee2ed35794e9c7c6851c9f42680d2fa74
gerov12 <peque_boster@hotmail.com>
2021-10-06 18:19:12 -0300

    Cambio de nombre de directorio base a ".polycon"

--- lib/polycon/utils.rb
+++ lib/polycon/utils.rb
@@ -2,11 +2,11 @@ module Polycon
     module Utils
         def self.posicionar_en_polycon
             Dir.chdir(ENV["HOME"])
-            if Dir.exist?("polycon") #acordarse de poner el . a polycon
-                Dir.chdir("polycon") 
+            if Dir.exist?(".polycon")
+                Dir.chdir(".polycon") 
             else
-                Dir.mkdir("polycon")
-                Dir.chdir("polycon")
+                Dir.mkdir(".polycon")
+                Dir.chdir(".polycon")
             end
         end
     end
