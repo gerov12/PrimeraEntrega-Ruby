eb86e292bf502eeaa8645285d65b83b28dba416c
gerov12 <peque_boster@hotmail.com>
2021-12-16 13:36:55 -0300

    seeders y devise sin register

--- TPI/app/models/professional.rb
+++ TPI/app/models/professional.rb
@@ -3,12 +3,6 @@ class Professional < ApplicationRecord
     validates :name, presence: true, format: {with: /\A[a-zA-Z]+\z/, message: 'must contain letters only'}, uniqueness: { case_sensitive: false }, length: {maximum: 50}
 
     def can_be_deleted?
-        if appointments.any?
-            appointments.select do |appointment|
-                appointment.date > DateTime.now
-            end.empty? 
-        else
-            true
-        end
+        !appointments.where('date > ?', DateTime.now).exists?
     end
 end
--- TPI/db/seeds.rb
+++ TPI/db/seeds.rb
@@ -7,5 +7,18 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create(email: 'consultant@gmail.com', password: '123456')
-User.create(email: 'assistant@gmail.com', password: '123456', role: 1)
-User.create(email: 'administrator@gmail.com', password: '123456', role: 2)
+User.create(email: 'assistant@gmail.com', password: '123456', role: 'assistant')
+User.create(email: 'administrator@gmail.com', password: '123456', role: 'admin')
+
+Professional.create(name: 'Jose')
+Professional.create(name: 'Maria')
+Professional.create(name: 'Alfredo')
+
+Appointment.create(professional_id: 1, name: 'Hernan', surname: 'Lopez', phone: 12345567, date: DateTime.strptime("12/21/2021 14:15", "%m/%d/%Y %H:%M"))
+Appointment.create(professional_id: 1, name: 'Carla', surname: 'Simoni', phone: 12343367, date: DateTime.strptime("12/21/2021 10:15", "%m/%d/%Y %H:%M"))
+Appointment.create(professional_id: 1, name: 'Tomas', surname: 'Ramirez', phone: 12312567, date: DateTime.strptime("12/22/2021 15:00", "%m/%d/%Y %H:%M"))
+Appointment.create(professional_id: 2, name: 'Cristian', surname: 'Sanchez', phone: 12895567, date: DateTime.strptime("12/22/2021 14:45", "%m/%d/%Y %H:%M"))
+Appointment.create(professional_id: 2, name: 'Lucia', surname: 'Pietro', phone: 12345556, date: DateTime.strptime("12/27/2021 11:30", "%m/%d/%Y %H:%M"))
+Appointment.create(professional_id: 3, name: 'Ana', surname: 'Ortega', phone: 12387967, date: DateTime.strptime("12/27/2021 14:15", "%m/%d/%Y %H:%M"))
+Appointment.create(professional_id: 3, name: 'Ulises', surname: 'Gonzalez', phone: 12445567, date: DateTime.strptime("12/28/2021 09:00", "%m/%d/%Y %H:%M"))
+Appointment.create(professional_id: 3, name: 'Fernando', surname: 'Rossi', phone: 89655567, date: DateTime.strptime("12/29/2021 13:30", "%m/%d/%Y %H:%M"))
